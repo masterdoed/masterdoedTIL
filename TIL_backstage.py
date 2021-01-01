8a37333f15888f7e02f3c35147a1d042f0812a4c
masterdoed <masterdoed@doedspace.de>
2021-01-01 21:47:40 +0100

    sub tag extraction added

--- TIL_backstage.py
+++ TIL_backstage.py
@@ -14,8 +14,27 @@ class Backstage:
 		eventList=[]
 		html=requests.get(url).text
 		soup=BeautifulSoup(html, 'lxml')
+		#get all event DIV tags
 		events=soup.find_all(class_='teaser-item event')
-		
+		#get event media
+		# TO BE DONE (<img>)
+		#get event titles
+		for event in events:
+			#get event title + link
+			titles=event.findAll('h2', class_='pos-title')
+			for title in titles:
+				eventNames=title.findAll('a')
+				for eventName in eventNames:
+					print(eventName)
+			#get event date
+			datetimes=event.findAll('div', class_='element-date')
+			for datetime in datetimes:
+				dts=datetime.findAll('p')
+				for dt in dts:
+					print(dt)
+
+
+
 
 
 	

cc7a7ec1fa116dade10a03dcae79494776667cbc
masterdoed <masterdoed@doedspace.de>
2021-01-01 21:50:02 +0100

    data model description added to finder

--- TIL_ConcertFinder.py
+++ TIL_ConcertFinder.py
@@ -1,5 +1,8 @@
 import TIL_backstage
 
+
+#event data format: datetime|title|url|media|eventimlink|shopping
+
 bs_url="https://backstage.info/veranstaltungen-2/alle-veranstaltungen"
 bstage=TIL_backstage.Backstage()
 bstage.readBackstage(bs_url)
